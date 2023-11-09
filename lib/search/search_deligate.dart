2c26ca4233a4db33a1d5a6bc545496ce26ed7b90
Sohaila Daoud <63005434+sohailadaoud@users.noreply.github.com>
2023-11-09 19:37:04 +0200

    search bar UI.

--- lib/search/search_deligate.dart
+++ lib/search/search_deligate.dart
@@ -2,20 +2,32 @@ import 'package:flutter/material.dart';
 
 class SearchDeligate extends SearchDelegate {
   @override
+  ThemeData appBarTheme(BuildContext context) {
+    return Theme.of(context).copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(color: Colors.black));
+  }
+
+  @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
           onPressed: () {},
           icon: Icon(
             Icons.search,
-            color: Colors.pinkAccent,
+            color: Colors.white,
           ))
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return IconButton(onPressed: () {}, icon: Icon(Icons.close));
+    return IconButton(
+        onPressed: () {},
+        icon: Icon(
+          Icons.close,
+          color: Colors.white,
+        ));
   }
 
   @override
--- lib/search/search_screen.dart
+++ lib/search/search_screen.dart
@@ -10,43 +10,29 @@ class SearchScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.black,
-      // appBar: AppBar(
-      //     elevation: 0,
-      //     backgroundColor: Colors.black,)
-      //,
-      body: InkWell(
-        onTap: () {
-          showSearch(context: context, delegate: SearchDeligate());
-        },
-        child: Container(
-          margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-          decoration: BoxDecoration(
-            color: Colors.black26,
-            borderRadius: BorderRadius.circular(10.0),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
           ),
-          padding: EdgeInsets.all(10.0),
-          child: TextField(
-            controller: searchBarController,
-            decoration: InputDecoration(
-              hintText: 'Search ',
-              hintStyle: TextStyle(color: Colors.grey),
-              border: InputBorder.none,
-              // prefixIcon: Icon(
-              //   Icons.search,
-              //   color: Colors.white,
-              // )
+          TextFormField(
+            onTap: () {
+              showSearch(context: context, delegate: SearchDeligate());
+            },
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+            decoration: new InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.grey, width: 2.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              labelText: "Search",
+              labelStyle: TextStyle(color: Colors.grey),
             ),
-            //  autofocus: true,
           ),
-        ),
+        ],
       ),
     );
   }
-
-  //TextEditingController searchBarController = TextEditingController();
-  bool searchBarVisibile = false;
-
-  void searchBar() {}
 }
 
 //Widget build(BuildContext context) {
@@ -96,3 +82,25 @@ class SearchScreen extends StatelessWidget {
 //           )),
 //     );
 //   }
+//
+
+//  // InkWell(
+//           //   onTap: () {
+//           //     showSearch(context: context, delegate: SearchDeligate());
+//           //   },
+//           //
+//           //   child: TextField(
+//           //     controller: searchBarController,
+//           //     decoration: InputDecoration(
+//           //
+//           //       hintText: 'Search ',
+//           //       hintStyle: TextStyle(color: Colors.grey),
+//           //       border: InputBorder.none,
+//           //       prefixIcon: Icon(
+//           //         Icons.search,
+//           //         color: Colors.white,
+//           //       )
+//           //     ),
+//           //     //  autofocus: true,
+//           //   ),
+//           // ),
